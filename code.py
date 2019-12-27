290557b8c37bc238f644b37812dec2f769c1a303
JulietteGerbaux <juliette.gerbaux@mines-paristech.fr>
2019-12-27 15:28:53 +0100

    euler explicite et show_solver et test sur exp

--- code.py
+++ code.py
@@ -1,4 +1,26 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+def solve_euler_explicit(f, x0, dt, n = 100):
+  t = np.linspace(0, dt*(n-1), n)
+  x = np.zeros(n)
+  x[0] = x0
+  for j in range(1, n):
+    x[j] = x[j-1] + dt*f(x[j-1])
+  return t, x
+
+def show_solver(solver, f, x0, dt, n =100):
+  t, x = solver(f, x0, dt, n)
+  plt.close()
+  plt.plot(t, x)
+  plt.show()
+
+def exp_solver(solver, dt, xf = 5, x0 = 1):
+  t, x = solver(lambda x : x, x0, dt, int(xf//dt))
+  plt.close()
+  plt.plot(t, x)
+  plt.plot(t, [np.exp(i) for i in t])
+  plt.show()
 
 
-def solve_euler_explicit(f, x0, dt):
-  pass
+
